e6b95201b2c1e7db6906593040b6352ed2765be4
step0035 <77708985+step0035@users.noreply.github.com>
2022-11-16 17:26:41 +0800

    [Tools] Modify flashing script to enable flashing to specific address (#9)
    * [Tools] Modify flash.sh to flash to specific address
    - Address field is optional, if not given, flash to default 0x08000000
    - ./flash.sh <serial port> <path to image> <address>
    
    * [Readme] Update the linux imagetool readme

--- tools/AmebaZ2/Image_Tool_Linux/flash.sh
+++ tools/AmebaZ2/Image_Tool_Linux/flash.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Usage
-# ./flash.sh <Device Port> <Path to image>
+# ./flash.sh <Device Port> <Path to image> <address[optional]>
 # ./flash.sh /dev/ttyUSB0 /path/to/image
 
 if [ -z "$1" ]
@@ -22,6 +22,14 @@ IMAGE=$2
 rm -rf log*
 sudo ./Ameba_ImageTool -add device $1
 sudo ./Ameba_ImageTool -set baudrate 1500000
+if [ -n "$3" ]
+then
+  echo "Flashing to address $3"
+  sudo ./Ameba_ImageTool -set address $3
+else
+  # if no address is given, use default address 0x08000000
+  echo "Flashing to address 0x08000000"
+fi
 sudo ./Ameba_ImageTool -set image $IMAGE
 sudo ./Ameba_ImageTool -show
 read -p "Check if the settings are correct, then enter UART DOWNLOAD mode
