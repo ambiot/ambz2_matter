af1d5df22fe66f2615b689eddb1655a331b405d8
xshuqun <77948861+xshuqun@users.noreply.github.com>
2023-01-25 11:21:34 +0800

    [wifi] Rework matter wifi api (#48)
    * Add wrapper function for matter to call function in ameba
    * Support Open Security and WEP Security Mode

--- component/common/application/matter/common/port/matter_wifis.h
+++ component/common/application/matter/common/port/matter_wifis.h
@@ -10,6 +10,7 @@ extern "C" {
 #endif
 
 #include <wifi_conf.h>
+#include <lwip_netconf.h>
 
 #define JOIN_HANDSHAKE_DONE (uint32_t)(1 << 7)
 extern uint32_t rtw_join_status;
@@ -25,7 +26,19 @@ void chip_connmgr_set_callback_func(chip_connmgr_callback p, void *data);
 void matter_scan_networks(void);
 void matter_scan_networks_with_ssid(const unsigned char *ssid, size_t length);
 void matter_get_scan_results(rtw_scan_result_t *result_buf, uint8_t scanned_num);
-
+int matter_wifi_connect(
+    char              *ssid,
+    rtw_security_t    security_type,
+    char              *password,
+    int               ssid_len,
+    int               password_len,
+    int               key_id,
+    void              *semaphore);
+int matter_wifi_disconnect(void);
+int matter_wifi_on(rtw_mode_t mode);
+int matter_wifi_set_mode(rtw_mode_t mode);
+int matter_wifi_is_connected_to_ap(void);
+uint8_t matter_lwip_dhcp(uint8_t idx, uint8_t dhcp_state);
 #ifdef __cplusplus
 }
 #endif
