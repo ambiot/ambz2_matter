029502be566e6422e6ed6ef9b1793833542a3863
step0035 <step0035@e.ntu.edu.sg>
2023-02-23 17:03:11 +0800

    [portinglayer] more cleanups

--- component/common/application/matter/core/matter_interaction.h
+++ component/common/application/matter/core/matter_interaction.h
@@ -7,4 +7,3 @@
 void PostDownlinkEvent(const AppEvent * aEvent);
 CHIP_ERROR matter_interaction_start_downlink(void);
 CHIP_ERROR matter_interaction_start_uplink(void);
-// void matter_interaction_onoff_handler(AppEvent *aEvent);
