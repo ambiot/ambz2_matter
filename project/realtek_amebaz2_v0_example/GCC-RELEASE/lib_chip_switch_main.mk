eba9e06fcc30c4b7e43e6d13c75dbccecec1607b
step0035 <step0035@e.ntu.edu.sg>
2023-04-10 10:52:56 +0800

    [build] add matter_api to main mk

--- project/realtek_amebaz2_v0_example/GCC-RELEASE/lib_chip_switch_main.mk
+++ project/realtek_amebaz2_v0_example/GCC-RELEASE/lib_chip_switch_main.mk
@@ -227,6 +227,8 @@ SRC_CPP += $(CHIPDIR)/examples/platform/ameba/ota/OTAInitializer.cpp
 SRC_CPP += $(CHIPDIR)/examples/providers/DeviceInfoProviderImpl.cpp
 SRC_CPP += $(CHIPDIR)/examples/platform/ameba/shell/launch_shell.cpp
 
+SRC_CPP += $(BASEDIR)/../../../component/common/application/matter/api/matter_api.cpp
+
 #lib_version
 VER_C += $(TARGET)_version.c
 
