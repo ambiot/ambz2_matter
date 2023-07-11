e78b9deb1e28b3f439ecf5f16f814d9a271288eb
step0035 <step0035@e.ntu.edu.sg>
2023-07-11 18:46:26 +0800

    [portinglayer][api] add api to open basic commissioning window - also add a bufSize argument to matter_get_fabric_indexes to avoid overflow

--- component/common/application/matter/api/matter_api.cpp
+++ component/common/application/matter/api/matter_api.cpp
@@ -1,22 +1,36 @@
-#include <platform_stdlib.h>
-#include "matter_api.h"
-
 #include <platform/CHIPDeviceLayer.h>
 #include <app/server/Server.h>
+
+#include <platform_stdlib.h>
+#include "matter_api.h"
+// #include <app/server/CommissioningWindowManager.h>
 
 bool matter_server_is_commissioned()
 {
     return (chip::Server::GetInstance().GetFabricTable().FabricCount() != 0);
 }
 
-void matter_get_fabric_indexes(uint16_t *pFabricIndexes)
+void matter_get_fabric_indexes(uint16_t *pFabricIndexes, uint8_t bufSize)
 {
     size_t i = 0;
     for (auto it = chip::Server::GetInstance().GetFabricTable().begin(); 
         it != chip::Server::GetInstance().GetFabricTable().end(); ++it)
     {
-            ChipLogError(DeviceLayer, "Fabric Index = %d", it->GetFabricIndex());
-            pFabricIndexes[i] = it->GetFabricIndex();
-            i++;
+        if (bufSize < i)
+        {
+            // out of buffer space
+            ChipLogError(DeviceLayer, "Returning... buffer too small");
+            return;
+        }
+        ChipLogProgress(DeviceLayer, "Fabric Index = %d", it->GetFabricIndex());
+        pFabricIndexes[i] = it->GetFabricIndex();
+        i++;
     }
+}
+
+void matter_open_basic_commissioning_window()
+{
+    chip::DeviceLayer::PlatformMgr().LockChipStack();
+    chip::Server::GetInstance().GetCommissioningWindowManager().OpenBasicCommissioningWindow();
+    chip::DeviceLayer::PlatformMgr().UnlockChipStack();
 }
--- component/common/application/matter/api/matter_api.h
+++ component/common/application/matter/api/matter_api.h
@@ -1,4 +1,5 @@
 #pragma once
 
 bool matter_server_is_commissioned(void);
-void matter_get_fabric_indexes(uint16_t *pFabricIndexes);
+void matter_get_fabric_indexes(uint16_t *pFabricIndexes, uint8_t bufSize);
+void matter_open_basic_commissioning_window(void);
