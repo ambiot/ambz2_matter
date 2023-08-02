ce67ca392d72753a1481c024d22680f1c08e075a
step0035 <77708985+step0035@users.noreply.github.com>
2023-08-02 10:05:36 +0800

    [build] add IcdManagementServer (#145)
    * [build] add IcdManagementServer
    
    * [build] add IcdManagementServer for portinglayer examples

--- component/common/application/matter/example/thermostat/lib_chip_thermostat_main.mk
+++ component/common/application/matter/example/thermostat/lib_chip_thermostat_main.mk
@@ -198,6 +198,7 @@ SRC_CPP += $(CHIPDIR)/src/app/server/OnboardingCodesUtil.cpp
 SRC_CPP += $(CHIPDIR)/src/app/server/Server.cpp
 SRC_CPP += $(CHIPDIR)/src/app/server/CommissioningWindowManager.cpp
 
+SRC_CPP += $(CHIPDIR)/src/app/icd/IcdManagementServer.cpp
 SRC_CPP += $(CHIPDIR)/src/app/icd/IcdMonitoringTable.cpp
 SRC_CPP += $(CHIPDIR)/src/app/util/attribute-size-util.cpp
 SRC_CPP += $(CHIPDIR)/src/app/util/attribute-storage.cpp
