e28adf571fb210c5e12632d0b0e0a3b855e214ee
xshuqun <77948861+xshuqun@users.noreply.github.com>
2023-08-24 13:48:05 +0800

    [mdns] update mdns libraries (#152)
    * fix compatibility issue with lwip_v2.1.2
    * mdns support to send response to interface 1 for concurrent mode

--- component/common/network/mDNS/mDNSPlatform.c
+++ component/common/network/mDNS/mDNSPlatform.c
@@ -1,6 +1,7 @@
 #include <lwip/sockets.h>
 #include <lwip/netif.h>
 extern struct netif xnetif[];
+extern uint8_t *LwIP_GetIP(struct netif *pnetif);
 
 /*-----------------------------------------------------------------------
  * Mandatory functions
@@ -34,4 +35,36 @@ char *mDNSPlatformHostname(void)
 #endif
 }
 
-/*-----------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------
+ * WEAK functions
+ *-----------------------------------------------------------------------*/
+
+
+uint8_t mDNSPlatformNetifIsUp(int idx)
+{
+	return netif_is_up(&xnetif[idx]);
+}
+
+#if LWIP_IPV6
+uint8_t *mDNSPlatformIP6Addr(int idx)
+{
+#if LWIP_VERSION_MAJOR >= 2
+	return (uint8_t *) & (xnetif[idx].ip6_addr[0].u_addr.ip6);
+#else
+	return (uint8_t *) & (xnetif[idx].ip6_addr[0].addr[0]);
+#endif
+}
+#endif
+
+char *mDNSPlatformIP1String(int type)
+{
+	static char ip_str[40];
+
+	if (type == AF_INET) {
+		uint8_t *ip = LwIP_GetIP(&xnetif[1]);
+		sprintf(ip_str, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+	}
+
+	return ip_str;
+}
+
