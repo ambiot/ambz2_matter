8e9a4fb1b499cd6dc013d02d06624247a65c3132
SirRu24 <mikaelnd001@outlook.com>
2023-09-15 14:46:30 +0800

    [portinglayer] edited lib_chip_refrigerator_main.mk

--- component/common/application/matter/example/refrigerator/lib_chip_refrigerator_main.mk
+++ component/common/application/matter/example/refrigerator/lib_chip_refrigerator_main.mk
@@ -206,6 +206,8 @@ SRC_CPP += $(CHIPDIR)/src/app/util/message.cpp
 SRC_CPP += $(CHIPDIR)/src/app/util/util.cpp
 SRC_CPP += $(CHIPDIR)/src/app/util/privilege-storage.cpp
 SRC_CPP += $(CHIPDIR)/src/app/reporting/Engine.cpp
+# SRC_CPP += $(CHIPDIR)/examples/all-clusters-app/all-clusters-common/src/laundry-washer-mode.cpp
+# SRC_CPP += $(CHIPDIR)/examples/all-clusters-app/all-clusters-common/src/laundry-washer-controls-delegate-impl.cpp
 
 SRC_CPP += $(shell cat $(CODEGENDIR)/cluster-file.txt)
 
@@ -222,8 +224,8 @@ SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/core/matter_core.cp
 SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/core/matter_interaction.cpp
 SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/core/matter_ota_initializer.cpp
 SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/example/refrigerator/example_matter_refrigerator.cpp
-SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/driver/opstate_driver.cpp
-SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/driver/washer_driver.cpp
+# SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/driver/opstate_driver.cpp
+# SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/driver/washer_driver.cpp
 SRC_CPP += $(SDKROOTDIR)/component/common/application/matter/example/refrigerator/matter_drivers.cpp
 
 #lib_version
