487fccb6953d2240be4b91b8aede79e2631abf2c
SirRu24 <mikaelnd001@outlook.com>
2023-10-23 11:09:30 +0800

    [shell] exclude ChipTestShutdown function if it is not CHIP_ALL_CLUSTERS_APP

--- component/common/application/matter/common/atcmd/atcmd_matter.c
+++ component/common/application/matter/common/atcmd/atcmd_matter.c
@@ -8,7 +8,9 @@
 #include <sys_api.h>
 #include "log_service.h"
 extern void ChipTest(void);
+#if CHIP_ALL_CLUSTERS_APP
 extern void ChipTestShutdown(void);
+#endif
 extern u32 deinitPref(void);
 #if CONFIG_ENABLE_OTA_REQUESTOR
 #if CONFIG_EXAMPLE_MATTER_CHIPTEST
@@ -75,7 +77,9 @@ void fATmattershell(void *arg)
 
 void fATreboot(void *arg)
 {
-    ChipTestShutdown();
+#if CHIP_ALL_CLUSTERS_APP
+	ChipTestShutdown();
+#endif
     sys_reset();
 }
 
