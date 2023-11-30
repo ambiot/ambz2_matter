d6cf7d801e669431f2ea721c73ff5f3b5b3d497d
xshuqun <xushuqun175@gmail.com>
2023-11-30 08:38:04 +0800

    [sdk] fix build error with ICDManagementServer
    * ICDManagementServer has been removed in SHA:6778d3e5088fb535238c5448889a47b0bf8db2d2

--- component/common/application/matter/example/light_dm/lib_chip_light_main.mk
+++ component/common/application/matter/example/light_dm/lib_chip_light_main.mk
@@ -199,7 +199,6 @@ SRC_CPP += $(CHIPDIR)/src/app/server/OnboardingCodesUtil.cpp
 SRC_CPP += $(CHIPDIR)/src/app/server/Server.cpp
 SRC_CPP += $(CHIPDIR)/src/app/server/CommissioningWindowManager.cpp
 
-SRC_CPP += $(CHIPDIR)/src/app/icd/ICDManagementServer.cpp
 SRC_CPP += $(CHIPDIR)/src/app/icd/ICDMonitoringTable.cpp
 SRC_CPP += $(CHIPDIR)/src/app/util/attribute-storage.cpp
 SRC_CPP += $(CHIPDIR)/src/app/util/attribute-table.cpp
