4fadce429d8583faaae2a57ea636dd4eaa650ce3
xshuqun <xushuqun175@gmail.com>
2024-06-15 10:35:10 +0800

    [sdk] fix pwm unable to start
    * As pwmout_period_us and hal_pwm_enable has been removed in pwmout_init, it requires calling pwmout_period_us and pwmout_start in application to successfully start pwm

--- component/common/application/matter/driver/dishwasher_driver.cpp
+++ component/common/application/matter/driver/dishwasher_driver.cpp
@@ -4,7 +4,10 @@
 void MatterDishwasher::Init(PinName pin)
 {
     mPwm_obj                        = (pwmout_t*) pvPortMalloc(sizeof(pwmout_t));
+
     pwmout_init(mPwm_obj, pin);
+    pwmout_period_us(mPwm_obj, 20000); //pwm period = 20ms
+    pwmout_start(mPwm_obj);
 }
 
 void MatterDishwasher::deInit(void)
--- component/common/application/matter/driver/fan_driver.cpp
+++ component/common/application/matter/driver/fan_driver.cpp
@@ -6,6 +6,8 @@ void MatterFan::Init(PinName pin)
 {
     mPwm_obj                        = (pwmout_t*) pvPortMalloc(sizeof(pwmout_t));
     pwmout_init(mPwm_obj, pin);
+    pwmout_period_us(mPwm_obj, 20000); //pwm period = 20ms
+    pwmout_start(mPwm_obj);
 }
 
 void MatterFan::deInit(void)
